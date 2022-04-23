1eb532b29ab6fb92ba7b7b490ccb62ed1c462d0b
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2022-04-23 12:29:04 +0100

    Add files via upload

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' =>'required|max:500',
+            'created_at' => 'required|date',
+            'updated_at' => 'required|date'
+        ]);
+
+
+        $post = new Post();
+        $post->artist = $request->input('artist');
+        $post->title = $request->input('title');
+        $post->likes = $request->input('likes');
+        $post->description = $request->input('description');
+        $post->img = $request->input('img');
+        $post->created_at = $request->input('created_at');
+        $post->updated_at = $request->input('updated_at');
+        $post->save();
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view ('admin.posts.show', [
+
+
+            'post' => $post
+        ]);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        return view ('admin.posts.edit', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $request->validate([
+            'title' => 'required',
+            'description' =>'required|max:500',
+            'created_at' => 'required|date',
+            'updated_at' => 'required|date'
+        ]);
+
+        $post->artist = $request->input('artist');
+        $post->title = $request->input('title');
+        $post->likes = $request->input('likes');
+        $post->description = $request->input('description');
+        $post->img = $request->input('img');
+        $post->created_at = $request->input('created_at');
+        $post->updated_at = $request->input('updated_at');
+        $post->save();
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
